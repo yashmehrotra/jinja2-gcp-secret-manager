4d40f390a05d9a5668609f6ed5ab0d636c216c9a
Yash Mehrotra <yashmehrotra95@gmail.com>
2021-02-02 14:46:40 +0530

    New release: 1.1.0 (#2)

--- setup.py
+++ setup.py
@@ -18,9 +18,16 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license='Apache 2.0',
     install_requires=[
-        'google-auth',
-        'google-cloud-secret-manager>=2.0.0',
-        'Jinja2>=2.11.2',
+        'Jinja2==2.11.3',
+        'google-cloud-secret-manager==2.2.0',
+        # These are dependencies of secret-manager package, had to put
+        # them here to make it work
+        'google-api-core[grpc]==1.25.1',
+        'grpc-google-iam-v1==0.12.3',
+        'proto-plus==1.13.0',
+        'libcst >= 0.2.5',
+        'protobuf==3.14.0',
+        'googleapis-common-protos==1.52.0',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
